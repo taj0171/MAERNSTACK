9664b49fb88931c0ca1f3cdc3bf5bc561558210d
taj <tajoffl8056@gmail.com>
2023-04-21 17:52:38 +0530

    updated modules file

--- modules.js
+++ modules.js
@@ -3,3 +3,14 @@ console.log(access);
 console.log(access.student,access.marks);
 const{student,marks}=require('./students');
 console.log(student,marks);
+
+const name="sherlock";
+console.log(name);
+const greet=(name)=>{
+    console.log(`hello,${kannan}`);
+}
+greet('Bahubali');
+greet('kattappa');
+global.setTimeout(()=>{
+    console.log('In the timeout');
+},5000);
