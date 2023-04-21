1307f92c047e8f1eedcdeb4000383f9a8362fa1b
taj <tajoffl8056@gmail.com>
2023-04-21 15:52:37 +0530

    commiittititint

---
+++ Students.js
@@ -0,0 +1,8 @@
+const student=['kannan','Ramya','bavi'];
+const marks =[56,67,44]
+console.log(student);
+module.exports='hello vanakkam';
+module.exports=student;
+module.exports={
+    student,marks
+};
---
+++ modules.js
@@ -0,0 +1,5 @@
+const access=require('./students');
+console.log(access);
+console.log(access.student,access.marks);
+const{student,marks}=require('./students');
+console.log(student,marks);
