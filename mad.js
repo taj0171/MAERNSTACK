64c92eaebf5d08343fcf838cfcfca748cf5e10d9
taj <tajoffl8056@gmail.com>
2023-04-21 15:47:10 +0530

    committting mad code

---
+++ mad.js
@@ -0,0 +1,11 @@
+const name="sherlock";
+console.log(name);
+const greet=(name)=>{
+    console.log('hello,${kannan}');
+}
+greet('Bahubali');
+greet('kattappa');
+global.setTimeout(()=>{
+    console.log('In the timeout');
+},5000);
+const int=
